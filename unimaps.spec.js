c9749d55fe1fd9b1f63710f3d2e8f2891a1fd9ca
Rafael <rafaelferreiracosta26@gmail.com>
2019-12-03 19:43:05 -0300

    fixing when checking if item was deleted

--- unimaps.spec.js
+++ unimaps.spec.js
@@ -2,22 +2,28 @@
 
 context('Location', () => {
   beforeEach(() => {
+	cy.visit('http://unimaps.herokuapp.com/')
+	cy.contains('Logar').click();
   })
 
-  it('cy.location() - get window.location', () => {
-  cy.visit('http://unimaps.herokuapp.com/')
-  cy.contains('Logar').click();
+  it('Adiciona pavilhao', () => {
+  
 
   cy.get('.btn-floating.btn-large.modal-trigger').click();
   cy.get('#criarPavilhao input[name="nome"]').type('reitoria {enter}')
   cy.contains('reitoria')
 
-  cy.contains('edit').click();
-  cy.get('#editarPavilhao input[name="nome"]').type('recepcao {enter}')
-  cy.contains('recepcao')
-
-  cy.contains('delete').click();
-  cy.contains('recepcao');
   })
+  
+  it('edita pavilhão', () => {
+	  cy.contains('edit').click();
+	  cy.get('#editarPavilhao input[name="nome"]').type('recepcao {enter}')
+	  cy.contains('recepcao')
+})
+	
+it('deleta pavilão', () => {
+	cy.contains('delete').click();
+	cy.get('recepcao').should('not.exist')
+})
 
 })
